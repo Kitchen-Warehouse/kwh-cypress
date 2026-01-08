e50d27acdae61b0f9ec06eda08c309910986bf45
Jia-A <155526414+JiyaAgrawal20@users.noreply.github.com>
2026-01-08 10:09:24 +0530

    refactor E2E test for dualImage link to include status code verification

--- cypress/e2e/homepage/heroBanner/dualImage.cy.js
+++ cypress/e2e/homepage/heroBanner/dualImage.cy.js
@@ -11,21 +11,47 @@ describe('Hero Banner Tests', () => {
     cy.get('.dualImage').should('be.visible')
   })
 
-  it('should have a working link when dualImage has one', () => {
+  it('should have a working link that returns 200 status when dualImage has one', () => {
     cy.get('.dualImage').first().then($el => {
       // Check if there's a link inside dualImage
       if ($el.find('a').length > 0) {
         cy.get('.dualImage a').first()
           .should('have.attr', 'href')
           .and('not.be.empty')
-        cy.get('.dualImage a').first().click()
+        
+        cy.get('.dualImage a').first().invoke('attr', 'href').then(href => {
+          // Test status code
+          cy.request({
+            url: href,
+            failOnStatusCode: false
+          }).then((response) => {
+            expect(response.status).to.eq(200)
+          })
+          
+          // Test navigation
+          cy.get('.dualImage a').first().click()
+          cy.url().should('include', href)
+        })
       }
       // Check if dualImage is wrapped in a link
       else if ($el.parent('a').length > 0) {
         cy.get('.dualImage').first().parent('a')
           .should('have.attr', 'href')
           .and('not.be.empty')
-        cy.get('.dualImage').first().parent('a').click()
+        
+        cy.get('.dualImage').first().parent('a').invoke('attr', 'href').then(href => {
+          // Test status code
+          cy.request({
+            url: href,
+            failOnStatusCode: false
+          }).then((response) => {
+            expect(response.status).to.eq(200)
+          })
+          
+          // Test navigation
+          cy.get('.dualImage').first().parent('a').click()
+          cy.url().should('include', href)
+        })
       }
     })
   })

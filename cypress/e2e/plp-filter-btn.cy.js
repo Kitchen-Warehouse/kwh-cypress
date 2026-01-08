c0dfb1965192443b673930bbd796759a66c02045
Kotesh-Compose <koteswara@compose.co.in>
2026-01-08 18:26:49 +0530

    Refactor: Improve filter button tests for closing the filters on button click

--- cypress/e2e/plp-filter-btn.cy.js
+++ cypress/e2e/plp-filter-btn.cy.js
@@ -12,39 +12,61 @@ describe("Product Listing Page Filter Button Tests", () => {
     cy.get('[data-link-text="air fryers"]').should("be.visible").click();
   });
 
-  it("should display the show/hide filters button", () => {
-    cy.get(".xl\\:flex .filters-btn-text").should("be.visible");
-  });
+    it("should display the show/hide filters button", () => {
+      cy.get(".xl\\:flex .filters-btn-text").should("be.visible");
+    });
 
-  it('should show "Show filters" text initially', () => {
+    it('should show "Show filters" text initially', () => {
+      cy.get(".xl\\:flex .filters-btn-text")
+        .find("span.lg\\:block")
+        .should("contain.text", "Show filters")
+        .and("be.visible");
+    });
+
+    it("should display filter icon on the button", () => {
+      cy.get(".xl\\:flex .filters-btn-text").find("svg").should("exist");
+    });
+
+    it("should click the show filters button", () => {
+      cy.get(".xl\\:flex .filters-btn-text").click();
+    });
+
+    it("should display filter panel with all filter options after clicking show filters button", () => {
+      // Click the show filters button
+      cy.get(".xl\\:flex .filters-btn-text").click();
+
+      // Verify the filter sidebar container is visible with all filter options
+      cy.get('[class*="ProductFilters_filterDesktopContainer"]')
+        .first()
+        .within(() => {
+          cy.contains("button span", "Brand")
+            .parent("button")
+            .should("have.attr", "data-headlessui-state", "open");
+
+          cy.get("ul li:has(label)").should("have.length", 20); // ~20 brands filters shown
+        });
+    });
+
+  it('should change button text to "Hide filters" after clicking', () => {
+    // Click the show filters button
+    cy.get(".xl\\:flex .filters-btn-text").click();
+    // Verify the button text changes to "Hide filters"
     cy.get(".xl\\:flex .filters-btn-text")
       .find("span.lg\\:block")
-      .should("contain.text", "Show filters")
+      .should("contain.text", "Hide filters")
       .and("be.visible");
   });
 
-  it("should display filter icon on the button", () => {
-    cy.get(".xl\\:flex .filters-btn-text").find("svg").should("exist");
-  });
-
-  it("should click the show filters button", () => {
-    cy.get(".xl\\:flex .filters-btn-text").click();
-  });
-
-  it("should display filter panel with all filter options after clicking show filters button", () => {
+  it("should hide filter panel after clicking hide filters button", () => {
     // Click the show filters button
     cy.get(".xl\\:flex .filters-btn-text").click();
+    // Click the hide filters button
+    cy.get(".xl\\:flex .filters-btn-text").click();
 
-    // Verify the filter sidebar container is visible with all filter options
-    cy.get('[class*="ProductFilters_filterDesktopContainer"]')
-      .first()
-      .within(() => {
-        cy.contains("button span", "Brand")
-          .parent("button")
-          .should("have.attr", "data-headlessui-state", "open");
-
-        cy.get("ul li:has(label)").should("have.length", 20); // ~20 brands filters shown
-      });
+    // Verify the filter panel is hidden
+    cy.get('[class*="ProductFilters_filterDesktopContainer"]').should(
+      "not.exist"
+    );
   });
 
 });
